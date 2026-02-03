3d938f45d12b23ddb2344eb99005b78ad16baaa4
Esther <106287733+estheroluwabuyi@users.noreply.github.com>
2026-02-03 09:54:42 +0100

    Update subtext in JobStats to show progress
    Replaced interview rate subtext with a progress message.

--- src/helper/JobStats.js
+++ src/helper/JobStats.js
@@ -14,8 +14,8 @@ export function getThirdStat(stats) {
       return {
         value: `${stats.interviews}`,
         label: `Interview${stats.interviews !== 1 ? 's' : '' }`,
-        
-        subtext: `${interviewRate}% interview rate`,
+       // subtext: `${interviewRate}% interview rate`,
+        subtext: `Progress happening!`,
         color: 'purple',
              }
     }
