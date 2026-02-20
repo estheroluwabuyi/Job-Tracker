cf83783001e9822ce40ebf677a2ed52c46c2368e
Esther Okiki <estherokiki15@gmail.com>
2026-02-20 07:45:52 +0100

    fix nav bar font sizes

--- src/components/header/Logo.jsx
+++ src/components/header/Logo.jsx
@@ -2,8 +2,11 @@ import logo from "/images/logo.png";
 
 function Logo() {
   return (
-    <div>
-      <img src={logo} alt="Logo" className="h-30 w-auto" />
+    <div className="flex items-center">
+      <img src={logo} alt="Logo" className=" h-30 w-auto rounded-2xl" />
+      <h1 className="text-primary text-[2.5rem] -ml-3 -mb-2 font-semibold font-space tracking-wide leading-loose">
+        JobLog
+      </h1>
     </div>
   );
 }
--- src/components/header/NavBar.jsx
+++ src/components/header/NavBar.jsx
@@ -1,13 +1,64 @@
+import { Link } from "react-router-dom";
+import {
+  LayoutDashboard,
+  Workflow,
+  CreditCard,
+  LogIn,
+  Rocket,
+} from "lucide-react";
 import Logo from "./Logo";
+
+const links = [
+  { label: "Features", to: "/features", icon: LayoutDashboard },
+  { label: "How It Works", to: "/how-it-works", icon: Workflow },
+  { label: "Pricing", to: "/pricing", icon: CreditCard },
+];
 
 function NavBar() {
   return (
-    <div className="bg-primary">
-      <div>
-        <Logo />
-        <h1 className="text-[">JobLog</h1>
+    <nav className="border-b border-border">
+      <div className="flex items-center justify-between max-w-[1500px] mx-auto">
+        {/* Logo */}
+        <Link to="/" className="flex items-center">
+          <Logo />
+        </Link>
+
+        {/* Main Links */}
+        <div className="hidden md:flex items-center gap-8">
+          {links.map((link) => {
+            const Icon = link.icon;
+            return (
+              <Link
+                key={link.label}
+                to={link.to}
+                className="flex items-center gap-2 text-[1.5rem]! tracking-wide text-text font-medium"
+              >
+                <Icon className="text-[1.5rem]" />
+                {link.label}
+              </Link>
+            );
+          })}
+        </div>
+
+        <div className="flex items-center gap-10">
+          <Link
+            to="/login"
+            className="flex items-center gap-2 text-[1.5rem]! tracking-wide text-text font-medium"
+          >
+            <LogIn />
+            Log In
+          </Link>
+
+          <Link
+            to="/signup"
+            className="flex items-center gap-2 text-[1.5rem]! bg-primary  tracking-wide text-bg font-medium  px-4 py-2 rounded-md"
+          >
+            <Rocket />
+            Get Started
+          </Link>
+        </div>
       </div>
-    </div>
+    </nav>
   );
 }
 
