62a4ebd60ffded7cbbd4a6c04b153b5c145f0522
Esther Okiki <estherokiki15@gmail.com>
2026-09-12 07:13:01 +0100

    refactor: remove commented-out code in DashboardLayout for cleaner structure

--- src/components/dashboard/DashboardLayout.jsx
+++ src/components/dashboard/DashboardLayout.jsx
@@ -1,38 +1,3 @@
-// import Sidebar from "./sidebar/Sidebar";
-// import DashboardNavbar from "./navbar/DashboardNavbar";
-
-// import { useState } from "react";
-// import CollapsedSidebar from "./sidebar/CollapasedSidebar";
-
-// function DashboardLayout({ children }) {
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-
-//   return (
-//     <div
-//       className={`grid h-screen bg-bg-muted transition-[grid-template-columns] duration-500 ease-linear ${
-//         sidebarOpen
-//           ? "grid-cols-[100%_1fr] sm:grid-cols-[275px_1fr]"
-//           : "grid-cols-[64px_1fr]"
-//       }`}
-//     >
-//       <aside className="overflow-x-hidden">
-//         {sidebarOpen ? (
-//           <Sidebar onClose={() => setSidebarOpen(false)} />
-//         ) : (
-//           <CollapsedSidebar onOpen={() => setSidebarOpen(true)} />
-//         )}
-//       </aside>
-
-//       <main className="h-screen overflow-y-auto">
-//         <DashboardNavbar />
-
-//         <div>{children}</div>
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default DashboardLayout;
 import Sidebar from "./sidebar/Sidebar";
 import DashboardNavbar from "./navbar/DashboardNavbar";
 
